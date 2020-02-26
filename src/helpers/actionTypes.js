7e19c8e11daa28458c19a81d0824483aa321ef90
alee0510 <ali.muksin0510@gmail.com>
2020-02-26 15:22:09 +0700

    setup files

---
+++ src/helpers/actionTypes.js
@@ -0,0 +1,10 @@
+// user authentication
+export const LOGIN = 'LOG_IN'
+export const LOG_OUT = 'LOG_OUT'
+export const REGISTER = 'REGISTER'
+
+// DOM action => action toogle from DOM interactions
+
+
+// stay login
+export const STAY_LOGIN = 'STAY_LOG_IN'
---
+++ src/pages/register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Register extends React.Component {
+    render () {
+        return (
+            <div className = 'login-container'>
+                Login
+            </div>
+        )
+    }
+}
+
+export default Register
