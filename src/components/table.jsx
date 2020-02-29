494bcadd2a6e8e0713fefe39724898fcb65c851b
alee0510 <ali.muksin0510@gmail.com>
2020-03-01 02:07:13 +0700

    table components initiazion

---
+++ src/components/table.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Table = (props) => {
+    return (
+        <div className = 'table-main-container'>
+            table
+        </div>
+    )
+}
+
+export default Table
