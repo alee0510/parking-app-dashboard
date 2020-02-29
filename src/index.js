6417cd52d1e07ac8c29b1193367750c1a366a66a
alee0510 <ali.muksin0510@gmail.com>
2020-02-29 20:39:45 +0700

    set-up redux and router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+// setup redux
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import ReduxThunk from 'redux-thunk'
 
 // import main component
 import Main from './main'
 
+// import style
+import './styles/index.scss'
+
 ReactDOM.render(
-    <Main />, document.getElementById('root')
-);
+    <BrowserRouter>
+        <Main/>
+    </BrowserRouter>
+    ,document.getElementById('root')
+)
