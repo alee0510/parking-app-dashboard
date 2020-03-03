f7d8b2dd2d128eeb9cd50f3f40203fc523c1ae43
alee0510 <ali.muksin0510@gmail.com>
2020-03-03 18:22:32 +0700

    add pages

--- src/pages/chart.jsx
+++ src/pages/chart.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 // import style
 import '../styles/chart.scss'
 
 class Chart extends React.Component {
     render () {
+        if (!localStorage.getItem('token')) return <Redirect to = '/'/>
         return (
             <div className = 'chart-main-container'>
                 Chart
