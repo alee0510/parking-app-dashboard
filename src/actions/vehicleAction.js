847b1bee4ee44796afe010073edaa868cbc518af
alee0510 <ali.muksin0510@gmail.com>
2020-03-08 10:53:51 +0700

    add more vehicle actions

--- src/actions/vehicleAction.js
+++ src/actions/vehicleAction.js
@@ -1,6 +1,7 @@
 import Axios from 'axios'
 import { GET_CAR_BRANDS, GET_CAR_TYPES, GET_TOTAL_CAR_BRAND,
-    GET_TOTAL_CAR_TYPE 
+    GET_TOTAL_CAR_TYPE, GET_TOTAL_MOTOR_BRANDS, GET_TOTAL_MOTOR_TYPES, 
+    GET_MOTOR_BRANDS, GET_MOTOR_TYPES
 } from '../helpers/actionTypes'
 import { API_URL_ADMIN } from '../helpers/apiUrl'
 
@@ -117,3 +118,109 @@ export const getPrevCarTypes = (id, limit) => {
 }
 
 // MOTOR BRANDS
+export const getTotalMotorBrand = () => {
+    return async (dispacth) => {
+        try {
+            const { data } = await Axios.get(API_URL_ADMIN + `/vehicle/motor/brands/total`)
+            dispacth({
+                type : GET_TOTAL_MOTOR_BRANDS,
+                payload : data
+            })
+        } catch (err) {
+            console.log(err.response ? err.response.data : err)
+        }
+    }
+}
+export const getInitialMotorBrands = (limit) => {
+    return async (dispacth) => {
+        try {
+            const { data } = await Axios.get(API_URL_ADMIN + `/vehicle/motor/brands/?limit=${limit}`)
+            dispacth({
+                type : GET_MOTOR_BRANDS,
+                payload : data
+            })
+        } catch (err) {
+            console.log(err.response ? err.response.data : err)
+        }
+    }
+}
+export const getNextMotorBrands = (id, limit) => {
+    return async (dispacth) => {
+        try {
+            const { data } = await Axios.get(API_URL_ADMIN + `/vehicle/motor/brands/next/?id=${id}&limit=${limit}`)
+            dispacth({
+                type : GET_MOTOR_BRANDS,
+                payload : data
+            })
+        } catch (err) {
+            console.log(err.response ? err.response.data : err)
+        }
+    }
+}
+export const getPrevMotorBrands = (id, limit) => {
+    return async (dispacth) => {
+        try {
+            const { data } = await Axios.get(API_URL_ADMIN + `/vehicle/motor/brands/prev/?id=${id}&limit=${limit}`)
+            dispacth({
+                type : GET_MOTOR_BRANDS,
+                payload : data.reverse()
+            })
+        } catch (err) {
+            console.log(err.response ? err.response.data : err)
+        }
+    }
+}
+// MOTOR TYPES
+export const getTotalMotorType = () => {
+    return async (dispacth) => {
+        try {
+            const { data } = await Axios.get(API_URL_ADMIN + `/vehicle/motor/types/total`)
+            dispacth({
+                type : GET_TOTAL_MOTOR_TYPES,
+                payload : data
+            })
+        } catch (err) {
+            console.log(err.response ? err.response.data : err)
+        }
+    }
+}
+export const getInitialMotorBrands = (limit) => {
+    return async (dispacth) => {
+        try {
+            const { data } = await Axios.get(API_URL_ADMIN + `/vehicle/motor/types/?limit=${limit}`)
+            dispacth({
+                type : GET_MOTOR_TYPES,
+                payload : data
+            })
+        } catch (err) {
+            console.log(err.response ? err.response.data : err)
+        }
+    }
+}
+export const getNextMotorBrands = (id, limit) => {
+    return async (dispacth) => {
+        try {
+            const { data } = await Axios.get(API_URL_ADMIN + `/vehicle/motor/types/next/?id=${id}&limit=${limit}`)
+            dispacth({
+                type : GET_MOTOR_TYPES,
+                payload : data
+            })
+        } catch (err) {
+            console.log(err.response ? err.response.data : err)
+        }
+    }
+}
+export const getPrevMotorBrands = (id, limit) => {
+    return async (dispacth) => {
+        try {
+            const { data } = await Axios.get(API_URL_ADMIN + `/vehicle/motor/types/prev/?id=${id}&limit=${limit}`)
+            dispacth({
+                type : GET_MOTOR_TYPES,
+                payload : data.reverse()
+            })
+        } catch (err) {
+            console.log(err.response ? err.response.data : err)
+        }
+    }
+}
+
