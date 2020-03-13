edad6ac6922758a7c8e934806984944a63498ff6
alee0510 <ali.muksin0510@gmail.com>
2020-03-14 03:58:15 +0700

    bugs fixed

--- src/pages/setting.jsx
+++ src/pages/setting.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IconButton, TextField } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
 
 // import icons
 import EditIcon from '@material-ui/icons/Edit'
